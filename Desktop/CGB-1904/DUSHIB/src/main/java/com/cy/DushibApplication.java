453f301c33a9c3e0e6aafbeb3b2647ecb10b28c6
miaoshihao <2272846381@qq.com>
2019-07-18 11:15:07 +0800

    项目

---
+++ Desktop/CGB-1904/DUSHIB/src/main/java/com/cy/DushibApplication.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DushibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DushibApplication.class, args);
+	}
+
+}
